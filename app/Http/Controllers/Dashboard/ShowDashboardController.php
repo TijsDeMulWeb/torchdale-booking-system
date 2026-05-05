220345f46cb774a66a166889793f21a30ee1025c
Tijs De Mul <tijs.de.mul@gmail.com>
2026-05-05 21:40:41 +0200

    Add app layout and use dashboard view
    Change ShowDashboardController to return the 'dashboard' view (instead of 'dashboard.show'). Add a new Blade layout component at resources/views/components/layouts/app.blade.php with a basic HTML skeleton and Vite asset includes. Create an empty resources/views/dashboard.blade.php as the view target for the controller.

--- app/Http/Controllers/Dashboard/ShowDashboardController.php
+++ app/Http/Controllers/Dashboard/ShowDashboardController.php
@@ -12,6 +12,6 @@ class ShowDashboardController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return view('dashboard.show');
+        return view('dashboard');
     }
 }

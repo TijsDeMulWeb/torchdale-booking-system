005fafc91fa012190450b9a2cb90e4db039b096f
Tijs De Mul <tijs.de.mul@gmail.com>
2026-05-05 21:16:52 +0200

    Add login view/layout/controller; remove welcome
    Introduce an invokable ShowLoginController that returns the new login.show view. Add a reusable auth layout component (resources/views/components/layouts/auth.blade.php) and a simple login view (resources/views/login/show.blade.php) that uses the layout. Remove the default resources/views/welcome.blade.php and update routes/web.php to point to the new login controller/route.

---
+++ resources/views/login/show.blade.php
@@ -0,0 +1,3 @@
+<x-layouts.auth>
+    Hi there! Please log in to access the dashboard.
+</x-layouts.auth>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Login\ShowLoginController;
 use Illuminate\Support\Facades\Route;
 
-
+Route::middleware('guest')->group(function () {
+    Route::get('/', ShowLoginController::class)->name('login.show');
+});

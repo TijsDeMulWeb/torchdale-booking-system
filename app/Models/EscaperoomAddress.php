463558f5beaa79a319010f98833e206809b1c572
Tijs De Mul <tijs.de.mul@gmail.com>
2026-05-06 18:30:01 +0200

    Use Fillable attribute on EscaperoomAddress
    Import and apply the Illuminate\Database\Eloquent\Attributes\Fillable attribute on the EscaperoomAddress model to declare mass-assignable fields. Sets fillable fields: escaperoom_id, street, house_number, postal_code, city, country_id, is_primary, replacing the need for a $fillable property.

--- app/Models/EscaperoomAddress.php
+++ app/Models/EscaperoomAddress.php
@@ -3,7 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 
+#[Fillable(['escaperoom_id', 'street', 'house_number', 'postal_code', 'city', 'country_id', 'is_primary'])]
 class EscaperoomAddress extends Model
 {
     public function escaperoom()

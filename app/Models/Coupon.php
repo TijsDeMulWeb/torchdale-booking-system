7a85ef167e9ca62aed65cca2fc4e0e48f53ee0b0
Tijs De Mul <tijs.de.mul@gmail.com>
2026-05-28 17:33:24 +0200

    Added fillable fields

--- app/Models/Coupon.php
+++ app/Models/Coupon.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 
+#[Fillable(['name', 'description', 'code', 'discount_type', 'discount_value', 'valid_from', 'valid_until', 'usage_limit', 'times_used'])]
 class Coupon extends Model
 {
     use SoftDeletes;

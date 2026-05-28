dfe186724c9aefe6b9d7ef1d3d33979a3074c1b0
Tijs De Mul <tijs.de.mul@gmail.com>
2026-05-28 18:42:08 +0200

    Fix coupon edit link route
    Corrects the route name in the coupon index view from `products.edit` to `coupons.edit` so the "Wijzigen" link navigates to the proper coupon edit page. (Updated resources/views/coupon/index.blade.php)

--- resources/views/coupon/index.blade.php
+++ resources/views/coupon/index.blade.php
@@ -104,7 +104,7 @@
 
                                                 <div class="flex items-center justify-end gap-4">
 
-                                                    <a href="{{ route('products.edit', $coupon->id) }}"
+                                                    <a href="{{ route('coupons.edit', $coupon->id) }}"
                                                         class="text-indigo-600 hover:text-indigo-900 dark:text-indigo-400 dark:hover:text-indigo-300">
                                                         Wijzigen
                                                         <span class="sr-only">, {{ $coupon->name }}</span>

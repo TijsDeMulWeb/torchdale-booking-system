2bd8ed6ec8447d4a7abb4271c3008427466deab4
Tijs De Mul <tijs.de.mul@gmail.com>
2026-05-31 15:05:29 +0200

    Add Fillable attribute to RoomPrice model
    Use PHP 8 attribute-based fillable declaration on RoomPrice by adding #[Fillable([...])] with fields: room_id, base_price, price, player_amount, day_of_week, vat_percentage. Also import Illuminate\Database\Eloquent\Attributes\Fillable and Illuminate\Database\Eloquent\SoftDeletes (SoftDeletes is imported but not applied in this change).

--- app/Models/RoomPrice.php
+++ app/Models/RoomPrice.php
@@ -3,7 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
+
+#[Fillable(['room_id', 'base_price', 'price', 'player_amount', 'day_of_week', 'vat_percentage'])]
 class RoomPrice extends Model
 {
     public function room()

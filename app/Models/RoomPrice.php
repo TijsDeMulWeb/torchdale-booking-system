80d22068e87baac5ba88322ec7c1d5dad6187e3c
Tijs De Mul <tijs.de.mul@gmail.com>
2026-05-31 15:47:55 +0200

    Add payment_location to room prices
    Add a payment_location enum to room_prices (values: 'online', 'location') with default 'online', and make it mass-assignable by adding it to RoomPrice's Fillable attribute. Also normalize anonymous class brace formatting in the migration file.

--- app/Models/RoomPrice.php
+++ app/Models/RoomPrice.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 
-#[Fillable(['room_id', 'base_price', 'price', 'player_amount', 'day_of_week', 'vat_percentage'])]
+#[Fillable(['room_id', 'base_price', 'price', 'player_amount', 'day_of_week', 'vat_percentage', 'payment_location'])]
 class RoomPrice extends Model
 {
     public function room()
--- database/migrations/2026_05_31_121447_create_room_prices_table.php
+++ database/migrations/2026_05_31_121447_create_room_prices_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -19,6 +18,7 @@ return new class extends Migration
             $table->integer('player_amount');
             $table->integer('day_of_week');
             $table->integer('vat_percentage');
+            $table->enum('payment_location', ['online', 'location'])->default('online');
             $table->timestamps();
             $table->softDeletes();
         });

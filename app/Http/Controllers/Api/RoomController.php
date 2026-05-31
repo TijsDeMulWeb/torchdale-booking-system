b22834ae24a909f8c2ab5a53e764b691aff67dcd
Tijs De Mul <tijs.de.mul@gmail.com>
2026-05-31 22:26:17 +0200

    Add RoomController and register rooms route
    Introduce an invokable RoomController that returns the escaperoom's rooms as JSON (response format: {"success": true, "rooms": [...] }). Register a new GET /rooms route under the AuthenticateEscaperoom middleware. Also fix the ProductCategory controller import (rename ProductCategoriesController to ProductCategoryController) in routes/api.php.

---
+++ app/Http/Controllers/Api/RoomController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RoomController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $rooms = $request->escaperoom->rooms()->get();
+
+        return response()->json(['success' => true, 'rooms' => $rooms]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\Api\ProductCategoriesController;
+use App\Http\Controllers\Api\ProductCategoryController;
+use App\Http\Controllers\Api\RoomController;
 use App\Http\Controllers\ChatbotController;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Middleware\AuthenticateEscaperoom;
@@ -10,5 +11,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware([AuthenticateEscaperoom::class])->group(function () {
     Route::post('/chat', ChatbotController::class)->name('chat.index');
     Route::get('/products', ProductController::class)->name('products.index');
-    Route::get('/product-categories', ProductCategoriesController::class)->name('product-categories.index');
+    Route::get('/product-categories', ProductCategoryController::class)->name('product-categories.index');
+    Route::get('/rooms', RoomController::class)->name('rooms.index');
 });

5805588aa9ab767ed1e11a30914864bc46131273
Tijs De Mul <tijs.de.mul@gmail.com>
2026-06-05 17:24:57 +0200

    Create 2026_06_05_171153_create_invoices_table.php

---
+++ database/migrations/2026_06_05_171153_create_invoices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id')->constrained();
+            $table->foreignId('order_id')->constrained();
+            $table->string('mollie_invoice_id')->nullable()->unique();
+            $table->string('pdf_url')->nullable();
+            $table->string('source')->default('mollie');
+            $table->string('invoice_number')->unique();
+            $table->string('status');
+            $table->decimal('amount', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};

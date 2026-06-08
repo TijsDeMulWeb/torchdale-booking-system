327c491c4c678f6dfecdf355395e0bc332f8fbbf
Tijs De Mul <tijs.de.mul@gmail.com>
2026-06-08 21:29:25 +0200

    Send invitation email on user creation
    Use the authenticated user's escaperoom to set escaperoom_id, generate a random password, and create a temporary signed password-setup URL (48h). Import Mail, URL, Str and a UserInvitationMail mailable, then send the invitation email to the new user's address. This replaces the insecure use of the escaperoom name as the password and notifies the user with a setup link.

--- app/Http/Controllers/User/StoreUserController.php
+++ app/Http/Controllers/User/StoreUserController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUserRequest;
+use App\Mail\UserInvitationMail;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Str;
 
 class StoreUserController extends Controller
 {
@@ -14,11 +18,21 @@ class StoreUserController extends Controller
      */
     public function __invoke(StoreUserRequest $request)
     {
+        $escaperoom = auth()->user()->escaperoom;
+
         $user = new User();
         $user->fill($request->validated());
-        $user->escaperoom_id = auth()->user()->escaperoom_id;
-        $user->password = auth()->user()->escaperoom->name;
+        $user->escaperoom_id = $escaperoom->id;
+        $user->password = Str::random(40);
         $user->save();
+
+        $passwordSetupUrl = URL::temporarySignedRoute(
+            'passwordSetup.show',
+            now()->addHours(48),
+            ['user' => $user->id]
+        );
+
+        Mail::to($user->email)->send(new UserInvitationMail($user, $escaperoom, $passwordSetupUrl));
 
         return redirect()->route('users.index')->with('message', 'Gebruiker succesvol aangemaakt.');
     }

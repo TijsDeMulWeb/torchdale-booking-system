a75a2c6635ad68c766a59cbbd07bea0ed88349d0
Tijs De Mul <tijs.de.mul@gmail.com>
2026-06-08 11:07:36 +0200

    Add UnblockTimeSlotRangeRequest validation
    Introduce UnblockTimeSlotRangeRequest FormRequest to validate inputs for unblocking time slot ranges. authorize() returns true. Validation rules enforce: room_id as required integer, start_date as required date_format:Y-m-d, and end_date as required date_format:Y-m-d and after_or_equal:start_date.

---
+++ app/Http/Requests/UnblockTimeSlotRangeRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UnblockTimeSlotRangeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'room_id' => ['required', 'integer'],
+            'start_date' => ['required', 'date_format:Y-m-d'],
+            'end_date' => ['required', 'date_format:Y-m-d', 'after_or_equal:start_date'],
+        ];
+    }
+}

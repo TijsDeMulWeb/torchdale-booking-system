5a478fef71a3ab0742e53ce6c1e4985e73947ad5
Tijs De Mul <tijs.de.mul@gmail.com>
2026-06-10 19:34:55 +0200

    Add users/roles navigation component
    Add a new Blade component at resources/views/components/users/nav.blade.php that renders tabbed navigation for user management. It defines a $tabs array for "Gebruikers" and "Rollen & rechten", uses route() for links and request()->routeIs with pattern arrays to toggle Tailwind active/inactive classes, centralizing the users/roles navigation UI.

---
+++ resources/views/components/users/nav.blade.php
@@ -0,0 +1,20 @@
+@php
+    $tabs = [
+        ['route' => 'users.index', 'pattern' => ['users.index', 'users.create', 'users.edit'], 'label' => 'Gebruikers'],
+        ['route' => 'roles.index', 'pattern' => ['roles.index', 'roles.create', 'roles.edit'], 'label' => 'Rollen & rechten'],
+    ];
+@endphp
+
+<div class="mb-6 border-b border-gray-200 dark:border-white/10">
+    <nav class="-mb-px flex space-x-8">
+        @foreach ($tabs as $tab)
+            <a href="{{ route($tab['route']) }}" @class([
+                'border-b-2 px-1 pb-4 text-sm font-medium whitespace-nowrap',
+                'border-indigo-500 text-indigo-600 dark:border-indigo-400 dark:text-indigo-400' => request()->routeIs($tab['pattern']),
+                'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 dark:text-gray-400 dark:hover:border-white/20 dark:hover:text-white' => !request()->routeIs($tab['pattern']),
+            ])>
+                {{ $tab['label'] }}
+            </a>
+        @endforeach
+    </nav>
+</div>
